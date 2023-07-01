e2736cdac644936124e8236d5994cc0943fb2622
Yadira Avila <patry_95forever@hotmail.com>
2023-06-30 23:35:47 -0500

    API terminada

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
-from flask import Flask
-from query import  get_company_information
 import requests
+from flask import Flask, request, Response
+from query import  get_company_information
+from query_example import get_example
+import json
+import os
 
 app = Flask(__name__)
 
@@ -10,8 +13,16 @@ def hello_world():
 
 @app.route('/api/yahoo')
 def get_company():
-    symbol = "DIS"
-    return get_company_information(symbol)
+    symbol = request.args.get('ticker')
+    if symbol in None:
+        return Response(json.dumps({"error": "missing 'ticker' query-parameter"}), status=400, mimetype='application/json')
+    return Response(json.dumps(get_company_information(symbol)), status=200, mimetype='application/json')
+@app.route('/api/example')
+def get_product_and_price_average():
+   query = request.args.get('search_text')
+   if query is None:
+       return Response(json.dumps({"error": "missing 'search_text' query-parameter"}), status=400, mimetype='application/json')
+   return Response(json.dumps(get_example(query)), status=200, mimetype='application/json')
 
 
 if __name__ == '__main__':
--- query_example.py
+++ query_example.py
@@ -1,27 +1,45 @@
 import urllib.parse
 import requests
 
-user_agent = {'User-agent': 'Mozilla/5.0'}
+def get_example(search_text: str):
+    user_agent = {'User-agent': 'Mozilla/5.0'}
 
-keywords = 'renault clio'
-keywords = urllib.parse.quote(keywords)
+    keywords = search_text
+    keywords = urllib.parse.quote(keywords)
+    url = f"https://api.wallapop.com/api/v3/general/search?filters_source=search_box&keywords={keywords}&longitude=-3.69196&latitude=40.41956"
+    r = requests.get(url, headers=user_agent)
 
-url = f"https://api.wallapop.com/api/v3/cars/search?category_ids=100&keywords={keywords}&filters_source=suggester&longitude=-3.69196&latitude=40.41956"
+    print(r.json())
+    search_results = r.json()['search_objects']
+    price_sum = 0
+    num_elements = len(search_results)
 
-r = requests.get(url, headers=user_agent)
+    if num_elements == 0:
+        return {
+            "num_results": num_elements,
+            "price_avg": None,
+            "results": [],
+        }
 
-print(r.json())
-search_results = r.json()['search_objects']
-price_sum = 0
-elements = len(search_results)
+    results =[]
+    for r in search_results:
+        articulo = {
+            "price": r["price"],
+            "title": r["title"],
+            "description": r["description"]
+        }
 
-for r in search_results:
-    print(r["content"]["price"])
-    print(r["content"]["title"])
-    print(r["content"]["storytelling"])
-    print("--------------------------------")
 
-    price_sum = price_sum + r["content"]["price"]
+        results.append(articulo)
+        print("--------------------------------")
 
-    print(price_sum/elements)
+        price_sum = price_sum + r["price"]
 
+        price_avg = price_sum/num_elements
+
+
+        return {
+           "num_results": num_elements,
+            "price_avg": price_avg,
+            "results": results,
+        }
